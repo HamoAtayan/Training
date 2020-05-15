b605fbd70d13f95a8e21fa43fbfedce6e08d0b36
Hmayak Atayan <hmayak.atayan@webbfontaine.com>
2020-05-15 10:17:04 +0400

    first commit

---
+++ src/main/java/Print/If.java
@@ -0,0 +1,11 @@
+package Print;
+
+/**
+ * Created by Hmayak Atayan on  15, May, 2020
+ */
+public class If {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
